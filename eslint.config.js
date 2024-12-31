ca9c48e05ad1934a5d837832782b87c90c3663ee
Tatyana Venanzi <tatyanavenanzi@gmail.com>
2024-12-31 13:56:18 -0800

    deleted tests folder for FE

--- eslint.config.js
+++ eslint.config.js
@@ -7,10 +7,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 export default [
   { ignores: ['dist'] },
   {
-    files: ['**/*.{js,jsx, test.js}'],
-    env: {
-      jest: true,
-    },
+    files: ['**/*.{js,jsx, '],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
--- tests/ActiveBoard.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import ActiveBoard from '../src/components/ActiveBoard';
-
-test('renders ActiveBoard with title and owner', () => {
-    render(<ActiveBoard ActiveBoard={{ id: 1, title: "Test Board", owner: "John Doe", cards: [] }} />);
-    expect(screen.getByText(/Board Title | Test Board/i)).toBeInTheDocument();
-    expect(screen.getByText(/Board Owner | John Doe/i)).toBeInTheDocument();
-});
